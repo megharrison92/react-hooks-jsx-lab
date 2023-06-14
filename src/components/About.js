24a8b90e92d1122b6853dd02a9d7edee6c7ed3fe
Megan Harrison <mtharrison@alumni.fullsail.edu>
2023-06-14 11:40:05 -0500

    completed lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,14 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  //how do you get the image
+  return (
+  <div id="about">About
+    <h2>About Me</h2>
+    <p>Any content of your choosing</p>
+    <img src={image} alt="I made this"/>
+  </div>
+  )
 }
 
 export default About;
